83f9c95d2f5667b7c58abc14d99e854959590bac
Ace Alrich <hex0@live.com>
2021-02-25 21:31:05 +0000

    update static

--- allowanceJS/main.js
+++ allowanceJS/main.js
@@ -1,6 +1,23 @@
 const userList = getUsers()?.length > 0 ? getUsers() : [];
-const static = getStatic()?.length > 0 ? getStatic() : [];
+const static = getStatic() || {
+  period: 7,
+  amount: 10,
+  intrest: 0,
+};
+
 const $createNewUserModal = $("#createNewUserModal");
+
+$("#allowancePeriodField").val(static.period);
+$("#allowaceAmountField").val(static.amount);
+$("#allowaceIntrestField").val(static.intrest);
+
+$("#staticVariableForm").submit(function (event) {
+  updateStatic({
+    period: $(this)[0][0].value,
+    amount: $(this)[0][1].value,
+    intrest: $(this)[0][2].value,
+  });
+});
 
 $("#newUserForm").submit(function () {
   userList.push({
